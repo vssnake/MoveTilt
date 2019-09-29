3d7efb404ad902e0974998eebfb43a0da1c4d5f2
vssnake <unaicorreacruz@gmail.com>
2019-09-29 12:44:22 +0200

    use signinConfig kts. (Some problems with keystore) Add AppDistribution firebase

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,8 @@ buildscript {
         classpath("com.android.tools.build:gradle:3.6.0-alpha12")
         classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.50")
         classpath ("com.google.gms:google-services:4.3.2")
+        classpath ("com.google.firebase:firebase-appdistribution-gradle:1.0.0")
+
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
