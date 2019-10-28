b1b7dd446e5de97bfc5d404ba77b128e83bd6e4d
unai correa <unai@baturamobile.com>
2019-10-28 12:43:46 +0100

    implement library change to animeList feature

---
+++ app/src/main/java/com/uratxe/animelist/features/animelist/AnimeListEvents.kt
@@ -0,0 +1,15 @@
+package com.uratxe.animelist.features.animelist
+
+data class AnimeListData(val email: String,val nickName : String)
+
+
+sealed class AnimeListViewEvent  {
+    object RetrieveUserEvent : AnimeListViewEvent()
+    data class ProcessErrorEvent(val typeError : String) : AnimeListViewEvent()
+}
+
+sealed class AnimeListModelEvent{
+    object LaunchActivity : AnimeListModelEvent()
+    data class StartStopLocation(val start : Boolean) : AnimeListModelEvent()
+}
+
