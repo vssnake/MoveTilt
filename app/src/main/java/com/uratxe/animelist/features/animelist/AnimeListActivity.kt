3e23a56b79bb621d189a0a47a642f2f8abad7974
unai correa <unai@baturamobile.com>
2019-10-28 12:43:46 +0100

    implement library change to animeList feature

--- app/src/main/java/com/uratxe/animelist/features/animelist/AnimeListActivity.kt
+++ app/src/main/java/com/uratxe/animelist/features/animelist/AnimeListActivity.kt
@@ -2,11 +2,10 @@ package com.uratxe.animelist.features.animelist
 
 import android.os.Bundle
 import com.uratxe.movetilt.*
-import com.uratxe.mvit.BaseActivity
-import com.uratxe.mvit.BaseModels
+
 import kotlin.reflect.KClass
 
-class AnimeListActivity : BaseActivity<AnimeListViewModel, Extended>() {
+class AnimeListActivity : KoinProyectActivity<AnimeListViewModel, AnimeListData, AnimeListViewEvent, AnimeListModelEvent>() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,35 +20,13 @@ class AnimeListActivity : BaseActivity<AnimeListViewModel, Extended>() {
 
     }
 
-
-}
-
-
-sealed class AnimeListViewEvent  {
-    object RetrieveUserEvent : AnimeListViewEvent()
-    data class ProcessErrorEvent(val typeError : String) : AnimeListViewEvent()
-}
-
-
-
-sealed class DeliveryStatus {
-    class Delivered : DeliveryStatus()
-    class Delivering : DeliveryStatus()
-    class NotDelivered(val error: String) : DeliveryStatus()
-}
-
-
-class Extended(override val viewData: View, override val modelData: Model) : BaseModels<String, Extended.Direction, Extended.ContentType>(){
-
-    sealed class Direction  {
-        class Incoming(val from: String) : Direction()
-        class Outgoing(val status: DeliveryStatus) : Direction()
+    override fun onModelReceived(data: AnimeListData) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-    sealed class ContentType {
-        class Text(val body: String) : ContentType()
-        class Image(val url: String, val caption: String) : ContentType()
-        class Audio(val url: String, val duration: Int) : ContentType()
+    override fun onEventModelReceived(data: AnimeListModelEvent) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
+
 
 }
