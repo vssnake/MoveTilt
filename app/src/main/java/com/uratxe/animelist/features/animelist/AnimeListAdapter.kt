a3f782b0bdcbb6bcf59a3b4d93448be9baac1093
unai <unaicorreacruz@gmail.com>
2019-12-02 20:58:07 +0100

    Implement more scroll carret

--- app/src/main/java/com/uratxe/animelist/features/animelist/AnimeListAdapter.kt
+++ app/src/main/java/com/uratxe/animelist/features/animelist/AnimeListAdapter.kt
@@ -7,16 +7,45 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.uratxe.AnimeListQuery
+import com.uratxe.movetilt.PaginationScrollListener
 import com.uratxe.movetilt.R
+import kotlinx.android.synthetic.main.activity_anime_list.*
 import kotlinx.android.synthetic.main.viewholder_anime_list.view.*
 
-class AnimeListAdapter : RecyclerView.Adapter<AnimeListViewHolder>() {
+class AnimeListAdapter(val animeListViewModel: AnimeListViewModel) : RecyclerView.Adapter<AnimeListViewHolder>() {
 
     private val list = mutableListOf<AnimeListQuery.Medium>()
 
+    var isLoading = false
+    var isLastPage = false
+
+
     internal fun loadData (data : AnimeListQuery.Data){
+        isLoading = false
+        isLastPage = !data.page!!.pageInfo!!.hasNextPage!!
         list.addAll(data.page!!.media!!.filterNotNull())
         notifyDataSetChanged()
+    }
+
+
+    override fun onAttachedToRecyclerView(recyclerView: RecyclerView) {
+        super.onAttachedToRecyclerView(recyclerView)
+
+        recyclerView.addOnScrollListener(object : PaginationScrollListener(recyclerView) {
+            override fun isLastPage(): Boolean {
+                return isLastPage
+            }
+
+            override fun isLoading(): Boolean {
+                return isLoading
+            }
+
+            override fun loadMoreItems() {
+                isLoading = true
+                //you have to call loadmore items to get more data
+                animeListViewModel.onEventFromView(AnimeListViewEvent.OnMorePagesLoad)
+            }
+        })
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AnimeListViewHolder {
