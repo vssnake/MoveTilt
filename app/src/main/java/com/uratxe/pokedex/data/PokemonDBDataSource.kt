72bbbd578728e31bfd9b1809d22927e48ca5ee69
Iratxe Lafuente <iratxe@baturamobile.com>
2021-07-15 16:06:31 +0200

    - Bugfix

--- app/src/main/java/com/uratxe/pokedex/data/PokemonDBDataSource.kt
+++ app/src/main/java/com/uratxe/pokedex/data/PokemonDBDataSource.kt
@@ -1,4 +1,11 @@
 package com.uratxe.pokedex.data
 
+import com.uratxe.pokedex.domain.Pokemon
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emptyFlow
+
 class PokemonDBDataSource: PokemonDataSource {
+    override fun allPokemons(): Flow<List<Pokemon>> {
+        return emptyFlow()
+    }
 }
